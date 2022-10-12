b970b739ee37cb01b9dc9c63c1057c1a0f7996af
Marius Lundgård <studio@mariuslundgard.com>
2022-10-12 12:48:41 +0200

    chore: configure dev data paths

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -2,12 +2,7 @@ import {devCommand} from '../src/cli/dev'
 
 devCommand({
   cwd: process.cwd(),
-  dataPath: [
-    //
-    'etc/**/*.json',
-    '../pkg-utils/etc/**/*.json',
-    '../v3/etc/api/sanity/**/*.json',
-  ],
+  dataPath: ['etc/**/*.json', 'playground/*/etc/**/*.json'],
 }).catch((err) => {
   // eslint-disable-next-line no-console
   console.error(err)
