f5c8af6ac7a5901709e7fc61bb7381829e0fdedf
Binoy Patel <me@binoy.io>
2023-06-06 14:25:49 -0400

    feat: Support adding link to internal references

--- playground/ts/src/function.ts
+++ playground/ts/src/function.ts
@@ -1,3 +1,18 @@
+import {BaseInterface} from './interface'
+
+/**
+ * Test function with link to BaseInterface
+ *
+ * @public
+ * @param options - Base Interface Link out
+ *  {@link BaseInterface | BaseInterface}
+ *
+ * @returns string
+ */
+export function testFunctionLink(options: BaseInterface): string {
+  return `${options.id} ${options.foo}`
+}
+
 export function testFunction(a: number, b?: number): number {
   return a + (b ?? 0)
 }
