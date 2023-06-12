f895c9a2f14fdc64745e19e434d888bdec91f7a8
RitaDias <anarita151@gmail.com>
2023-06-12 13:36:51 +0200

    fix: fix active colour for light theme

--- src/react/app/nav/SyntaxTreeItem.ts
+++ src/react/app/nav/SyntaxTreeItem.ts
@@ -1,22 +1,31 @@
+import {blue} from '@sanity/color'
 import {TreeItem, Theme} from '@sanity/ui'
 import {memo} from 'react'
 import styled from 'styled-components'
 
 export const SyntaxTreeItem = memo(
   styled(TreeItem)(({theme}: {theme: Theme}) => {
-    const background = theme.sanity.color.selectable.default.enabled.bg2
+    const isThemeDark = theme.sanity.color.dark
+    const activeBackground = blue[50].hex
+    const focusBackground = theme.sanity.color.selectable.default.enabled.bg2
     const color = theme.sanity.color.selectable.default.hovered.fg
 
-    return `
-      // focuses solely on changing the "()" color
+    const selectedLight = `&[data-selected] > [role='treeitem'] {
+      background-color: ${activeBackground} !important
+    }`
+
+    return `  
+    // solely on changing the "()" color
       &:focus code:nth-child(2),
       & > a:focus code:nth-child(2) {
         color: ${color} !important;
       }
 
+      ${isThemeDark ? null : selectedLight}
+
       // change focus for api items trees
       & > a:focus {
-        background: ${background} !important;
+        background: ${focusBackground} !important;
       }
     `
   })
