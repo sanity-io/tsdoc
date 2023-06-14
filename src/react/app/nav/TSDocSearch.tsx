89ddfd4a75bf7f17ec31092305fee17d680140f4
RitaDias <anarita151@gmail.com>
2023-06-14 18:01:48 +0200

    refactor: re-add memberslug to tsdocsearch

--- src/react/app/nav/TSDocSearch.tsx
+++ src/react/app/nav/TSDocSearch.tsx
@@ -75,6 +75,9 @@ const SearchOption = forwardRef(function SearchOption(
       packageScope: symbol.package.scope || null,
       memberName: symbol.name,
       releaseVersion: latestMember ? latestMember.releaseVersion : '0.0.0',
+      // symbols currently don't have a slug, so we just use the name
+      // not sure if we want to add the slug?
+      memberSlug: symbol.name.toLowerCase(),
     }),
     [latestMember, symbol]
   )
