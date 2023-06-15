7ea2e1bb44d17ea4dfcdd8300c6eaf0c0f8dfcc8
Binoy Patel <me@binoy.io>
2023-06-15 09:24:12 -0400

    feat: Show Heading for Remarks (#15)

--- src/react/comment/CommentRemarks.tsx
+++ src/react/comment/CommentRemarks.tsx
@@ -1,5 +1,6 @@
 import {TSDocComment} from '@sanity/tsdoc'
 import {ReactElement} from 'react'
+import {H} from '../lib/ui'
 import {PortableText} from './PortableText'
 
 export function CommentRemarks(props: {data: TSDocComment}): ReactElement {
@@ -8,5 +9,10 @@ export function CommentRemarks(props: {data: TSDocComment}): ReactElement {
 
   if (!remarks?.content) return <></>
 
-  return <PortableText blocks={remarks.content} />
+  return (
+    <>
+      <H size={[-1, 0, 1, 2]}>Remarks</H>
+      <PortableText blocks={remarks.content} />
+    </>
+  )
 }
