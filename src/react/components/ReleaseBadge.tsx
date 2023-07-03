5d8cd910b7e5adf934afcf43d598f7f6cf4b19ac
RitaDias <anarita151@gmail.com>
2023-07-03 15:53:33 +0200

    feat: add labels to badges on articles (#25)
    * feat: add labels to badges on articles
    
    * refactor: add icon instead
    
    * chore: add experimental condition

--- src/react/components/ReleaseBadge.tsx
+++ src/react/components/ReleaseBadge.tsx
@@ -1,7 +1,9 @@
+import {InfoOutlineIcon} from '@sanity/icons'
 import {APIReleaseTag} from '@sanity/tsdoc'
-import {Card, CardTone, Text} from '@sanity/ui'
+import {Box, Card, CardTone, Flex, Text, Tooltip} from '@sanity/ui'
 import {ReactElement, useMemo} from 'react'
 import {useTextSize} from '../lib/ui'
+import {ReleaseBadgeTextWrapper} from './ReleaseBadgeTextWrapper'
 import {UnformattedCode} from './UnformattedCode'
 
 export function ReleaseBadge(props: {
@@ -19,11 +21,43 @@ export function ReleaseBadge(props: {
     return 'primary'
   }, [releaseTag])
 
+  const explanation: string = useMemo(() => {
+    if (releaseTag === 'alpha' || releaseTag === 'experimental')
+      return `This API is in very early stages of release. Breaking changes are not only expected but very likely.\n We strongly don't recommend using it in production.`
+    if (releaseTag === 'beta')
+      return `This API is in early stages of release. Breaking changes are expected.\n We don't recommend using it in production.`
+    if (releaseTag === 'public')
+      return 'This API is public and stable. Breaking changes are unlikely.'
+
+    return 'primary'
+  }, [releaseTag])
+
   return (
-    <Card padding={padding} radius={radius} tone={releaseTone}>
-      <Text muted size={useTextSize([-1, -1, 0])}>
-        <UnformattedCode>{`@${releaseTag || 'public'}`}</UnformattedCode>
-      </Text>
-    </Card>
+    <>
+      <Card padding={padding} radius={radius} tone={releaseTone}>
+        <Text muted size={useTextSize([-1, -1, 0])}>
+          <Flex align={'center'} gap={2}>
+            <UnformattedCode>{`@${releaseTag || 'public'}`}</UnformattedCode>
+
+            <Text muted size={0}>
+              <Tooltip
+                content={
+                  <Box padding={2} sizing="content">
+                    <ReleaseBadgeTextWrapper muted size={1}>
+                      {explanation}
+                    </ReleaseBadgeTextWrapper>
+                  </Box>
+                }
+                fallbackPlacements={['bottom', 'top', 'left']}
+                placement="right"
+                portal
+              >
+                <InfoOutlineIcon />
+              </Tooltip>
+            </Text>
+          </Flex>
+        </Text>
+      </Card>
+    </>
   )
 }
